559399b07927fc17c15146350971ea8556df0b50
Artak Kirakosyan <artakkirakosyan96@mail.ru>
2023-09-20 12:10:31 -0700

    Add contact class

---
+++ src/contact/contact.py
@@ -0,0 +1,17 @@
+from typing import Optional
+
+
+class Contact:
+    def __init__(
+            self,
+            first_name: str,
+            last_name: str,
+            phone_number: str,
+            email: Optional[str] = None,
+            address: Optional[str] = None
+    ):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.phone_number = phone_number
+        self.email = email
+        self.address = address
