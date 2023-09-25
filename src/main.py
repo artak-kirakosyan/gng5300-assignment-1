90357ea839976bec3d267c4ebcd0f073961fcd4b
Artak Kirakosyan <artakkirakosyan96@mail.ru>
2023-09-25 05:02:45 -0700

    9 - Add arg parser for initialization

--- src/main.py
+++ src/main.py
@@ -1,9 +1,22 @@
+import argparse
+
 from phone_book.phone_book import PhoneBook
 from phone_book.phone_book_controller import PhoneBookController
 
 
 def main():
-    phone_book = PhoneBook.from_file("/home/artak/Documents/python/gng5300-assignment-1/a.csv")
+    parser = argparse.ArgumentParser(description="Phonebook Application")
+    parser.add_argument("--file-path", type=str, help="Path to a CSV file to initialize the phonebook")
+
+    args = parser.parse_args()
+
+    if args.file_path:
+        file_path = args.file_path
+        phone_book = PhoneBook.from_file(file_path)
+        print(f"Phonebook initialized from '{file_path}'")
+    else:
+        phone_book = PhoneBook()
+        print("Empty phonebook initialized")
     controller = PhoneBookController(phone_book)
     controller.run()
 
